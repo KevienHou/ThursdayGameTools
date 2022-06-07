d3230a3f336b32e4867c158b167f1a69c88366a6
侯利聪 <dev_hlc@163.com>
2022-06-07 18:46:24 +0800

    新增两个工具

---
+++ Assets/ThirdComponent/ThursdayTool/MonoPlugs/MonoPlus.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using UnityEngine.Networking;
+
+public static class MonoPlus
+{
+    public static void SetActive(this Component com, bool flag)
+    {
+        if (com != null && com.gameObject.activeSelf != flag)
+        {
+            com.gameObject.SetActive(flag);
+        }
+    }
+
+
+    public static bool IsNullOrEmpty(this string str)
+    {
+        return string.IsNullOrEmpty(str);
+    }
+
+
+    /// <summary>
+    /// 异步重载
+    /// </summary>
+    /// <param name="asyncOp"></param>
+    /// <returns></returns>
+    public static TaskAwaiter GetAwaiter(this AsyncOperation asyncOp)
+    {
+        var tcs = new TaskCompletionSource<object>();
+        asyncOp.completed += obj => { tcs.SetResult(null); };
+        return ((Task)tcs.Task).GetAwaiter();
+    }
+
+
+  
+
+}
+
---
+++ Assets/ThirdComponent/ThursdayTool/PathCopyer/Editor/PathCopyer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEditor;
+
+using UnityEngine;
+
+public class PathCopyer : Editor
+{
+    //[MenuItem("GameObject/HLCTools/打印路径", false, 20)]
+    [MenuItem("GameObject/打印路径", false, 20)]
+    static void GetGObjPath()
+    {
+        Transform temTrans = Selection.transforms[0];
+        if (temTrans == null)
+        {
+            Debug.LogError("Wrong Path");
+        }
+        string path = temTrans.name;
+        path = GetPath(path, temTrans.parent);
+        Debug.Log(path + "     注意 已经复制到剪贴板 ");
+        GUIUtility.systemCopyBuffer = path;
+    }
+
+
+    [MenuItem("ThursdayTools/Copy Select Obj Path", false, 20)]
+    static void GetGObjPathTool()
+    {
+        if (Selection.transforms.Length <= 0)
+        {
+            Debug.LogError("Wrong Path");
+        }
+        Transform temTrans = Selection.transforms[0];
+        string path = temTrans.name;
+        path = GetPath(path, temTrans.parent);
+        Debug.Log(path + "     注意 已经复制到剪贴板 ");
+        GUIUtility.systemCopyBuffer = path;
+    }
+
+
+
+    static string GetPath(string s, Transform trans)
+    {
+        //if (trans != null)  //带 root
+        if (trans.parent != null)  //不带 root
+        {
+            s = trans.name + "/" + s;
+            return GetPath(s, trans.parent);
+        }
+        return s;
+    }
+
+
+
+
+
+}
